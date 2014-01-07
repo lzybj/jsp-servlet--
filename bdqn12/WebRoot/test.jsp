0396ae9585f553c757ecff79e29ba463afaca476
ilyj <i.lyj@me.com>
2014-01-07 10:02:08 +0800

    jsp与servlet之基于EL表达式2

---
+++ bdqn12/WebRoot/test.jsp
@@ -0,0 +1,7 @@
+<%@ page language="java" import="java.util.*" pageEncoding="utf-8"%>
+<%
+	//request.setCharacterEncoding("utf-8");
+	//out.println(request.getParameter("un") + "<br>");
+	//out.println(new String(request.getParameter("un").getBytes("ISO-8859-1"),"UTF-8"));
+ %>
+ ${param.un }
---
+++ bdqn12/src/com/bdqn/servlet/LoginAction.java
@@ -0,0 +1,75 @@
+package com.bdqn.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.bdqn.service.UsersService;
+
+public class LoginAction extends HttpServlet {
+
+	/**
+	 * Destruction of the servlet. <br>
+	 */
+	public void destroy() {
+		super.destroy(); // Just puts "destroy" string in log
+		// Put your code here
+	}
+
+	/**
+	 * The doGet method of the servlet. <br>
+	 *
+	 * This method is called when a form has its tag value method equals to get.
+	 * 
+	 * @param request the request send by the client to the server
+	 * @param response the response send by the server to the client
+	 * @throws ServletException if an error occurred
+	 * @throws IOException if an error occurred
+	 */
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String userName = request.getParameter("userName");
+		String passWord = request.getParameter("passWord");
+		UsersService us = new UsersService();
+		boolean isOK = us.isLogin(userName, passWord);
+		HttpSession session = request.getSession();//∆ÙSESSION
+		//Cookie myCookie = new Cookie();
+		if(isOK){
+			session.setAttribute("username",userName);
+			response.sendRedirect("succ.jsp");
+		}else{
+			response.sendRedirect("fail.jsp");
+		}
+	}
+
+	/**
+	 * The doPost method of the servlet. <br>
+	 *
+	 * This method is called when a form has its tag value method equals to post.
+	 * 
+	 * @param request the request send by the client to the server
+	 * @param response the response send by the server to the client
+	 * @throws ServletException if an error occurred
+	 * @throws IOException if an error occurred
+	 */
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		this.doGet(request, response);
+	}
+
+	/**
+	 * Initialization of the servlet. <br>
+	 *
+	 * @throws ServletException if an error occurs
+	 */
+	public void init() throws ServletException {
+		// Put your code here
+	}
+
+}
