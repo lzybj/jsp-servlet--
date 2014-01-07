5caf73f6e63647f053a4400fcb53941bbec88b1c
ilyj <i.lyj@me.com>
2014-01-07 10:05:24 +0800

    jsp与servlet之init-param与context-param

---
+++ bdqn14/WebRoot/index.jsp
@@ -0,0 +1,30 @@
+<%@ page language="java" import="java.util.*" pageEncoding="GB18030"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%
+String path = request.getContextPath();
+String basePath = request.getScheme()+"://"+request.getServerName()+":"+request.getServerPort()+path+"/";
+%>
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+  <head>
+    <base href="<%=basePath%>">
+    
+    <title>My JSP 'index.jsp' starting page</title>
+	<meta http-equiv="pragma" content="no-cache">
+	<meta http-equiv="cache-control" content="no-cache">
+	<meta http-equiv="expires" content="0">    
+	<meta http-equiv="keywords" content="keyword1,keyword2,keyword3">
+	<meta http-equiv="description" content="This is my page">
+	<!--
+	<link rel="stylesheet" type="text/css" href="styles.css">
+	-->
+  </head>
+  
+  <body>
+    <c:out value="${str }" default="在请求作用域中找不到str"></c:out><br>
+    <c:out value="${str2 }" default="在session作用域中找不到str2"></c:out><br>
+    <c:out value="${str3 }" default="在application作用域中找不到str3"></c:out><br>
+    
+  </body>
+</html>
---
+++ bdqn14/src/com/bdqn/servlet/ScopeServlet.java
@@ -0,0 +1,72 @@
+package com.bdqn.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class ScopeServlet extends HttpServlet {
+
+	/**
+	 * Destruction of the servlet. <br>
+	 */
+	public void destroy() {
+		super.destroy(); // Just puts "destroy" string in log
+		// Put your code here
+	}
+
+	/**
+	 * The doGet method of the servlet. <br>
+	 *
+	 * This method is called when a form has its tag value method equals to get.
+	 * 
+	 * @param request the request send by the client to the server
+	 * @param response the response send by the server to the client
+	 * @throws ServletException if an error occurred
+	 * @throws IOException if an error occurred
+	 */
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String str = "我在请求共享域中";
+		String str2 = "我在SESSION共享域中";
+		String str3 = "我在application共享域中";
+		request.setAttribute("str",str);
+		HttpSession session = request.getSession();
+		session.setAttribute("str2", str2);
+		this.getServletContext().setAttribute("str3",str3);
+		//转发
+		//request.getRequestDispatcher("index.jsp").forward(request, response);
+		//重定向
+		response.sendRedirect("index.jsp");
+	}
+
+	/**
+	 * The doPost method of the servlet. <br>
+	 *
+	 * This method is called when a form has its tag value method equals to post.
+	 * 
+	 * @param request the request send by the client to the server
+	 * @param response the response send by the server to the client
+	 * @throws ServletException if an error occurred
+	 * @throws IOException if an error occurred
+	 */
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		this.doGet(request, response);
+	}
+
+	/**
+	 * Initialization of the servlet. <br>
+	 *
+	 * @throws ServletException if an error occurs
+	 */
+	public void init() throws ServletException {
+		// Put your code here
+	}
+
+}
